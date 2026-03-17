b7d885b78456f7570e43e73a72058e3a401b8516
Michsen <xmichsen@gmail.com>
2026-03-17 19:45:47 +0100

    fix: show price of rolled items in case battle if player lost

--- src/components/CaseBattle.tsx
+++ src/components/CaseBattle.tsx
@@ -461,6 +461,9 @@ export function CaseBattle({
                         <span className="text-[10px] text-gray-500">
                           {skin.rarity.name}
                         </span>
+                        <span className="text-[10px] text-green-400 font-mono mt-0.5">
+                          ${computeSkinPrice(skin, WINNER_INDEX).toFixed(2)}
+                        </span>
                         {item && (
                           <span className="text-[10px] text-gray-400">
                             {item.isStatTrak && (
